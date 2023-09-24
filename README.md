16e651c18909fc88046701278d9d357299e807f9
Uaaron Sihan <uaaronsihan@gmail.com>
2023-09-24 16:27:01 -0700

    used Parcel for initial test-build

---
+++ README.md
@@ -0,0 +1,4 @@
+# MyFlix Client Side 
+
+- This app will implement React to create the frontend of our Myflix movie application
+- Currently the file structure has been created along with integration of the build tool Parcel
---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import { createRoot } from 'react-dom/client';
+
+// import statement to indicate necessary bundling of './index.scss'
+import "./index.scss";
+
+// Main component (will eventually use all the others)
+const MyFlixApplication = () => {
+    return (
+        <div className="my-flix">
+            <div>Good morning</div>
+        </div>
+    );
+};
+
+// Finds the root of your app
+const container = document.querySelector("#root");
+const root = createRoot(container);
+
+// Tells React to render your app in the root DOM element
+root.render(<MyFlixApplication />);
